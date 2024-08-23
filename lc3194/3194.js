f255e3190edace8878fb26da3178f6a3cb45b6e7
David Cosby <davjcosby@gmail.com>
2024-08-23 15:00:52 -0600

    technically not required

--- lc3194/3194.js
+++ lc3194/3194.js
@@ -8,16 +8,15 @@ var minimumAverage = function (nums) {
       return a - b;
     });
   
-    var averages = [];
+    //var averages = [];
     var smallestSoFar = 51; // 50 is the max
     for (i = 0; i < n / 2; i++) {
       const minElement = nums[i];
       const maxElement = nums[n - i - 1];
       const avg = (minElement + maxElement) / 2;
-      averages.push(avg);
+      //averages.push(avg);
       smallestSoFar = Math.min(smallestSoFar, avg)
     }
   };
   
-  minimumAverage([1, 2, 3, 4, 5]);
-  
+  minimumAverage([1, 2, 3, 4, 5]);  
